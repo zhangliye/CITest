faf9e42ef5b3e0ce6edb0ef2a8c0d78b97f2e7fb
U-A-IHPC\zhangly <zhangly@IHP-SL18-60055.A-IHPC.shared-svc.local>
2018-12-28 15:56:38 +0800

    first

---
+++ src/CITest.jl
@@ -0,0 +1,5 @@
+module CITest
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,7 @@
+using Test
+
+@info "This is for the testing"
+@testset "Foo Tests" begin
+    @test 9 == 9
+    @test 8 == 8
+end;
